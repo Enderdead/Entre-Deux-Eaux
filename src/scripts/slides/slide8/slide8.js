a52f0a4e02c4bcdf7075c4b13ca9b7d4206affd7
Kevin Despoulains <kevin.despoulains@gmail.com>
2019-11-12 22:10:46 +0100

    adding node_modules and new slides

---
+++ src/scripts/slides/slide8/slide8.js
@@ -0,0 +1,47 @@
+var slide8content = document.getElementById("slide8content")
+
+var enBref = "<p align='justify'>Souvent la gestion publique de l'eau va être réalisée par les municipalités en régie : le conseil municipal vote le tarif de l'eau et les comptes sont publics. Cela signifie que les investissements dans l'eau sont financés par la tarification au consommateur. Pour l’eau, c’est donc différent d'autres services publiques qui sont financés par impôts : <i>“On a un budget à part donc on doit être à l'équilibre. Et donc, le tarif est censé représenter à peu près le coût de l'eau et de distribution de l'eau”</i>, résume Simon Porcher, maître de conférence à l’institut d’administration des entreprises et spécialiste de réforme des services publics de l’eau en France. <i>“Quand on est en privé par contre on a différentes formules de contrats. Le contrat le plus générique, c'est la délégation de service public : un opérateur privé va gérer le service mais ne détient pas l'infrastructure. Le réseau d'eau reste public mais c’est une entreprise privée qui gère, souvent Veolia ou Suez. C’est elle qui va produire l'eau, la distribuer et envoyer la facture au consommateur.“</i></p>"
+
+var enDetail = "<p align='justify'>La DSP est un mode de gestion que l’on retrouve traditionnellement sur des services tels que la distribution de l’eau potable. Sa recette*, repose sur cinq critères. Il faut d’abord un <b>contrat</b>, signé entre une <b>autorité délégante</b> et un <b>délégataire</b>.</p>"
+enDetail += "<p align='justify'>Dans le domaine de l’eau, une autorité délégante, ce sont par exemple l’État, les Collectivités Territoriales ou des établissements publics d’aménagement et de gestion des eaux. Et du côté du délégataire on retrouve les entreprises privées ( Veolia, Suez ou Saur), les sociétés d’économie mixte ( Eau du Bassin Rennais, Brest Métropole), des associations (Association Française des Professionnels de l’Eau et des Déchets, EAU - Élus Association Usagers,...), des personnes physiques (n’importe quel citoyen) ou morales de droit public (que sont par exemple les collectivités territoriales incarnées par le Préfet). </p>"
+enDetail += "<p align='justify'>Deux critères encore pour définir la DSP : la <b>gestion du service public</b> que délègue la DSP sur la base d’une <b>rémunération liée à l’exploitation de ce service</b>. Ces deux derniers éléments sont essentiels: au coeur du contrat, ce sont eux qui feront de la DSP un exercice vertueux pour la défense de l’ intérêt général ou pas. </p>"
+enDetail += "<p align='justify'>*Une définition issue de l’ouvrage <i>Services Publics délégués au privé : à qui profite le deal?</i> de la journaliste Isabelle Jarjaille.</p>"
+enDetail += "<p align='justify'><b>Voilà pour les critères de la DSP, mais celle-ci peut revêtir diverses formes :</b></p>"
+enDetail += "<p align='justify'><b> - Le contrat de concession</b></p>"
+enDetail += "<p align='justify'>Le concessionnaire assure la responsabilité financière de la délégation. La société privée s’engage sur l’investissement initial puis la gestion de l’équipement, et se remboursera en percevant les recettes du service public.</p>"
+enDetail += "<p align='justify'><b> - L'affermage</b></p>"
+enDetail += "<p align='justify'>Dans cette situation, l’entreprise exploite et prend soin du réseau, mais la collectivité décide et finance les investissements. La commune reste la propriétaire des équipements tandis que la société privée entretient le réseau d’eau potable.</p>"
+enDetail += "<p align='justify'><b> - La régie intéressée</b></p>"
+enDetail += "<p align='justify'>Ici, la personne publique confie la gestion de la distribution de l’eau au régisseur. Ce dernier gère la relation avec les usagers, réalise les travaux courants, tout en agissant pour le compte de la collectivité. C’est lui qui fait fonctionner le service et sera rémunéré en fonction de certains résultats d’exploitation.</p>"
+enDetail += "<p align='justify'><b> - La gérance</b></p>"
+enDetail += "<p align='justify'>C’est comme pour la régie intéressée sauf qu’ici, la rémunération est fixe, quels que soient les résultats d’exploitation du service d’eau. </p>"
+enDetail += "<p class='ps-slide8' align='justify'><b>PS:</b> la DSP ne doit pas être confondue avec le partenariat public-privé. Dans ce dernier, la rémunération est assurée par la collectivité qui paie un loyer. Le partenaire privé ne porte pas la responsabilité si arrive un risque d’exploitation.</p>"
+
+var loi = "<p align='justify'>Les communes de France ont des obligations à tenir quand il s’agit d‘eau potable. C’est écrit dans la loi. Et plus précisément, dans le code général des collectivités territoriales (<a href='https://www.legifrance.gouv.fr/affichCodeArticle.do;jsessionid=CB2E5764EA7A894ED96B80B39874D9C8.tpdjo03v_2?idArticle=LEGIARTI000022478501&cidTexte=LEGITEXT000006070633&dateTexte=20140130&categorieLien=id&oldAction=rechCodeArticle' target='_blank'>L. 2224-7-1 du CGCT</a>). C’est lui qui pose le cadre de l’action des communes dans le domaine de l’eau.</p>"
+loi += "<p align='justify'>Concernant la production, le transport et le stockage de l’eau, les communes possèdent une autre compétence, facultative cette fois-ci. </p>"
+loi += "<p align='justify'>Selon le site <a href='https://www.vie-publique.fr' target='_blank'>vie-publique.fr</a>, les communes sont tenues de fixer un schéma de distribution d’eau potable. Ce plan détermine les zones desservies par le réseau de distribution qui doit contenir un descriptif détaillé des ouvrages de transport et de distribution de l’eau potable. Autrement dit, tout ce qui permet de préserver les réseaux et de limiter les fuites : assurer l’entretien des réseaux, conduire leur renouvellement et les réparations nécessaires à leur bon fonctionnement. Tout cela doit garantir la continuité de service et la qualité de l’eau distribuée, sans perdre de vue l’objectif suivant : conserver un prix de l’eau acceptable pour l’usager.</p>"
+loi += "<p align='justify'>D’ailleurs, tous les ans, les maires doivent remettre un rapport public sur le prix et la qualité du service. </p>"
+loi += "<div class='ps-slide8'><p align='justify'><b>Le saviez-vous ?</b> L’eau est un bien commun et vital. Ainsi, en juillet 2010, l’Organisation des Nations Unies (ONU) a reconnu le droit à l’eau potable comme un droit fondamental. Deux ans plus tard, les parlementaires européens ont suivi avec une résolution sur la mise en oeuvre d’une législation européenne relative à l’eau.</p>"
+loi += "<p align='justify'><b>Et en France Alors ?</b> Une loi sur la mise en oeuvre effective du droit à l’eau potable et à l'assainissement a été adoptée par l’Assemblée nationale en juin 2016, mais le Sénat entre-temps, est passé par là : vidée de son contenu par le retrait de l’article permettant son financement, cette loi n’est pas retournée devant le Parlement lors de la précédente législature (Gouvernement Hollande). Aucun article n’a donc été adopté. Cette proposition devrait à nouveau être déposée. Mais pour l’instant, selon <a href='http://www.coalition-eau.org/nos-actions/plaidoyer/droit-a-leau-en-france/ ' target='_blank'>Coalition Eau</a> (mouvement des ONG françaises engagées pour l’accès à l’eau et à l’assainissement pour tous), la nouvelle législature fait des ronds dans l’eau...</p>"
+loi += "<p align='justify'>Ce qui n’est pas le cas concernant la mise en oeuvre  la loi NOTRe du 7 août 2015. Elle prévoit que le transfert des compétences « eau et assainissement » vers les communautés de communes et les communautés d’agglomération, sera obligatoire à compter du 1er janvier 2020.</p></div>"
+
+slide8content.innerHTML = enBref;
+
+function changeSlide8Content(mode) {
+    if (mode == "bref") {
+        slide8content.innerHTML = enBref;
+        document.getElementById("enbref-slide8").style.backgroundColor = '#AAAAAACC'
+        document.getElementById("endetail-slide8").style.backgroundColor = '#FFFFFF55'
+        document.getElementById("loi-slide8").style.backgroundColor = '#FFFFFF55'
+    } else if (mode == "detail") {
+        slide8content.innerHTML = enDetail;
+        document.getElementById("endetail-slide8").style.backgroundColor = '#AAAAAACC'
+        document.getElementById("enbref-slide8").style.backgroundColor = '#FFFFFF55'
+        document.getElementById("loi-slide8").style.backgroundColor = '#FFFFFF55'
+    } else {
+        slide8content.innerHTML = loi;
+        document.getElementById("loi-slide8").style.backgroundColor = '#AAAAAACC'
+        document.getElementById("endetail-slide8").style.backgroundColor = '#FFFFFF55'
+        document.getElementById("enbref-slide8").style.backgroundColor = '#FFFFFF55'
+    }
+};
